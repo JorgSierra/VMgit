fbb8c38b1613154a740a94a48ef4ea910cf60d00
JorgSierra <jorge.sierrar@gmail.com>
2022-10-30 23:40:19 -0500

    chage if statement

--- activateApache.sh
+++ activateApache.sh
@@ -2,7 +2,7 @@
 
 chequeoActivo=$(systemctl status apache2 | grep running | wc -l)
 
-if [[ $chequeoActivo ]]; then
+if [[ $chequeoActivo -ge 1 ]]; then
     echo "Apache activo"
 else
     echo "Apache inactivo"
