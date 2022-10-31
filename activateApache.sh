c017dee66636cdede4aedad917ed99499acd5e25
JorgSierra <jorge.sierrar@gmail.com>
2022-10-31 07:12:34 -0500

    Agregando detalles a apache y subiendo personajes

--- activateApache.sh
+++ activateApache.sh
@@ -3,8 +3,11 @@
 chequeoActivo=$(systemctl status apache2 | grep running | wc -l)
 
 if [[ $chequeoActivo -ge 1 ]]; then
-    echo "Apache activo"
+    echo "Todo esta funcionando perfectamente"
 else
-    echo "Apache inactivo"
+	echo "----------------------------->"
+    echo "Apache2 status: Inactivo"
+	echo "Iniciando apache2"
     sudo systemctl start apache2
+	echo "Apache2 iniciado"
 fi
